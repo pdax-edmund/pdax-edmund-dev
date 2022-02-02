ff4c196b7c124bfb9ab271652835dfbcae7991af
pdax-edmund <91948647+pdax-edmund@users.noreply.github.com>
2022-02-02 23:12:57 +0800

    Initial commit of package updates, index.js file and package.json file

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log('server running on port: http://localhost:' + PORT);
+});
